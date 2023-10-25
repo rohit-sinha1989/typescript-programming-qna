6c31b81d8d01d38e48b931185838c199d018110d
rohit-sinha1989 <42764266+rohit-sinha1989@users.noreply.github.com>
2023-10-25 13:40:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-programming-qna
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/rohit-sinha1989/typescript-programming-qna)
---
+++ index.ts
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>
+<br />
+<h2>1. Sum of Two Numbers</h2>
+<a href="javascript: void(0)" >
+<i class="la la-trash" (click)="sumOfTwoNumber(user)">Click</i>
+</a>
+`;
+
+function sumOfTwoNumber()
+{
+
+}
